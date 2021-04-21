995454779bc5a98c2ede5929ca6a53b7adfabd1e
Trace Brandt <trpbr92@gmail.com>
2021-04-21 14:19:43 -0500

    films GET route created

---
+++ server/routes/films.router.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const pool = require('../modules/pool');
+const router = express.Router();
+
+/**
+ * GET route template
+ */
+router.get('/', (req, res) => {
+    const query = `SELECT * FROM "films" ORDER BY "title" ASC;`;
+    pool.query(query)
+    .then(result => {
+        res.send(result.rows);
+    })
+    .catch(err => {
+        console.log('ERROR in GET films', err);
+        res.sendStatus(500)
+    })
+});
+
+
+
+module.exports = router;
