f8bf71b3b39c595bfe11bb5c30b4ec92f49291c1
Trace Brandt <trpbr92@gmail.com>
2021-04-21 14:36:58 -0500

    filmsReducer created

---
+++ src/redux/reducers/films.reducer.js
@@ -0,0 +1,10 @@
+const filmsReducer = (state = [], action) => {
+    switch (action.type) {
+        case 'SET_FILMS':
+            return action.payload;
+        default:
+            return state;
+    }
+}
+
+export default filmsReducer;
