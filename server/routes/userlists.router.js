432802574c9f6d891a50dcabb482ec7ade7b1e31
Trace Brandt <trpbr92@gmail.com>
2021-04-26 11:31:43 -0500

    movies displaying in watchlist

--- server/routes/userlists.router.js
+++ server/routes/userlists.router.js
@@ -3,7 +3,9 @@ const pool = require('../modules/pool');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    const query = `SELECT * FROM "userlists"`;
+    const query = `SELECT "user".id, "films".title, "films".description, "films".poster_url FROM "userlists"
+JOIN "user" ON "user".id = "userlists".user_id
+JOIN "films" ON "films".id = "userlists".film_id;`;
     pool.query(query)
     .then(result => {
         res.send(result.rows);
--- src/components/WatchListPage/WatchListPage.jsx
+++ src/components/WatchListPage/WatchListPage.jsx
@@ -6,18 +6,38 @@ import {useDispatch, useSelector} from 'react-redux';
 
 function WatchListPage(){
 const dispatch = useDispatch();
+const history = useHistory();
 const lists = useSelector(store => store.userlists);
 
     useEffect(() => {
         dispatch({type: 'FETCH_USER_LISTS'});
       }, []);
 
+    //   const filmProfile = (id) => {
+    //       console.log('watchListPage to filmProfile');
+    //       history.push(`/profile/${id}`);
+    //   }
+
     return(
 
         <>
+        <main>
         <h1>WatchList</h1>
       
-                    <h3>{JSON.stringify(lists)}</h3>
+                    {/* <h3>{JSON.stringify(lists)}</h3> */}
+                        <section className="films">
+      {lists.map(list => {
+        return (
+          <div key={list.id}>
+            <h3>{list.title}</h3>
+            <img height={300} width={200} src={list.poster_url} alt={list.title} onClick={()=>filmProfile(list.id)}/>
+           <p><button>Add To Seen</button> <button>Delete</button></p>
+           
+          </div>
+        )
+      })}
+    </section>
+    </main>
                 
                    
         </>
